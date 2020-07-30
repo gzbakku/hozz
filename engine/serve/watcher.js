7df6d2f7ba5c1fbbddd6be966679bbbb2be5c1ae
gzbakku <gzbakku@gmail.com>
2020-07-30 15:45:12 +0530

    now listing for enter input for reset in server api

--- engine/serve/watcher.js
+++ engine/serve/watcher.js
@@ -3,11 +3,18 @@ const chokidar = require('chokidar');
 module.exports = {
 
   init:(cwd)=>{
+
     chokidar.watch(cwd)
     .on('change',async (path)=>{
       common.tell("restarting / hozzifying");
       restart();
     });
+
+    process.stdin.on("data",(key)=>{
+      common.tell("restarting / hozzifying");
+      restart();
+    });
+
   }
 
 }
